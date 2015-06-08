8419b9025e8d9472682c6d51903fa3b3b42f9835
ADINA <ADINA@ADINA-T510>
2015-06-08 17:33:44 +0300

    fddsf

--- ass3/src/view/logInGui.java
+++ ass3/src/view/logInGui.java
@@ -48,6 +48,7 @@ public class logInGui extends JFrame{
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		System.out.println("hi");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(400, 200, 300, 300);
 		this.setSize(500, 300);
