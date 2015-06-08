fe595b01d0aac9ce0d58ef2f09b53416417a259f
nofargido <nofargido90@walla.com>
2015-06-08 16:20:05 +0300

    llllll

--- ass3/src/view/userMainMenuGUI.java
+++ ass3/src/view/userMainMenuGUI.java
@@ -77,7 +77,7 @@ public class userMainMenuGUI extends JFrame {
 		this.setSize(500, 500);
 		initialize();
 		this.setVisible(true);
-		System.out.println("nofarrrrr");
+		System.out.println("nofarrrrrr");
 
 	}
 	private void initialize() {
